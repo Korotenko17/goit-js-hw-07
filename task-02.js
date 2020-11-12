aa8d2969389182a8c7ca04b9135d13f5904f666f
Korotenko17 <66269882+Korotenko17@users.noreply.github.com>
2020-11-12 20:30:18 +0200

    добавил всё

---
+++ task-02.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const ingredients = [
+    "Картошка",
+    "Грибы",
+    "Чеснок",
+    "Помидоры",
+    "Зелень",
+    "Приправы"
+  ];
+
+  const ulList = document.getElementById("ingredients");
+  const foodIngredients = ingredients.forEach(ingredient => {
+    const items = document.createElement("li");
+    items.innerHTML = ingredient;
+    ulList.append(items);
+  });
+
+  
---
+++ task-05.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const input = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
+
+input.oninput = function () {
+    if (input.value === '') {
+       nameOutput.innerHTML = 'незнакомец';
+    } else{
+    nameOutput.innerHTML = input.value;
+    }
+}
